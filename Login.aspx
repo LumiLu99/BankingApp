c57ff4c5d52dc0346139112c6a36973b859be785
LumiLu99 <yenwen_lu@hotmail.com>
2024-06-29 12:30:16 +0800

    First customer login draft

--- Login.aspx
+++ Login.aspx
@@ -11,12 +11,22 @@
         <div>
             Welcome to Richard always call Darren Bank</div>
         <p>
-            Username:</p>
-        <p>
-            Password:</p>
-        <p>
-            <asp:Button ID="Button1" runat="server" Text="Login" />
+            Username:<asp:TextBox ID="txtUser" runat="server" OnTextChanged="username_TextChanged"></asp:TextBox>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
         </p>
+        <p>
+            <asp:RequiredFieldValidator ID="rfvUsername" runat="server" ErrorMessage="Please enter your username."></asp:RequiredFieldValidator>
+        </p>
+        <p>
+            Password:<asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+        </p>
+        <p>
+            <asp:RequiredFieldValidator ID="rfvPassword" runat="server" ControlToValidate="password" ErrorMessage="Please enter your password."></asp:RequiredFieldValidator>
+        </p>
+        <p>
+            <asp:Button ID="Btnlogin" runat="server" Text="Login" OnClick="loginBtn_Click" />
+        </p>
+        <asp:Label ID="lblUserError" runat="server" Text="Invalid username or password."></asp:Label>
     </form>
 </body>
 </html>
--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,14 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class Login : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void Btnlogin_Click(object sender, EventArgs e)
     {
+        string username = txtUser.Text.Trim();
+        string password = txtPassword.Text;
+        bool isValid = ValidateUser(txtUser.Text, txtPassword.Text);
 
+        if (isValid)
+        {
+            Response.Redirect("Home.aspx");
+        }
+        else
+        {
+            lblUserError.Text = "Invalid Username or Password";
+            txtUser.Text = "";
+            txtPassword.Text = "";
+        }
+    }
+
+    private bool ValidateUser(string username, string password)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionStringName"].ConnectionString;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string sql = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Username", username);
+            command.Parameters.AddWithValue("@Password", password);
+
+            try
+            {
+                connection.Open();
+                int count = (int)command.ExecuteScalar(); // Executes the query and returns the first column of the first row in the result set
+
+                if (count > 0)
+                {
+                    return true; // User credentials are valid
+                }
+                else
+                {
+                    return false; // User credentials are not valid
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while validating user credentials.", ex);
+            }
+        }
     }
 }
+
