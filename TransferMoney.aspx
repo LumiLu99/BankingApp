859273442ba49ba5c84b696cf67338b537507f0f
LumiLu99 <yenwen_lu@hotmail.com>
2024-07-06 16:46:06 +0800

    Added Validation for all pages, fixed Back button stopped by validation, fund sender and receiver are able to generate statement correctly

--- TransferMoney.aspx
+++ TransferMoney.aspx
@@ -21,16 +21,28 @@
         <br />
         Account number to transfer to:<br />
         <asp:TextBox ID="accNumber" runat="server" Height="18px" TextMode="Number" Width="117px"></asp:TextBox>
+        &nbsp;<asp:Label ID="lblAccValid" runat="server" ForeColor="Red" Visible="False"></asp:Label>
         <br />
+        <asp:RequiredFieldValidator ID="rfvAccNum" runat="server" ControlToValidate="accNumber" ErrorMessage="Please insert the account number" ForeColor="Red"></asp:RequiredFieldValidator>
+        <br />
+        <asp:RegularExpressionValidator ID="revAccNum" runat="server" ControlToValidate="accNumber"
+        ErrorMessage="Account number must be a 7-digit number" ValidationExpression="^\d{7}$"
+        ForeColor="Red"></asp:RegularExpressionValidator>
         <br />
         Amount:<br />
         <asp:TextBox ID="transferAmt" runat="server" Height="16px"></asp:TextBox>
+        <br />
+        <asp:RequiredFieldValidator ID="rfvAmount" runat="server" ControlToValidate="transferAmt" ErrorMessage="Please insert the amount" ForeColor="Red"></asp:RequiredFieldValidator>
+        <br />
+        <asp:RegularExpressionValidator ID="revAmount" runat="server" ControlToValidate="transferAmt"
+        ErrorMessage="Amount must not exceed 8 digits including 2 decimal places"
+        ValidationExpression="^\d{1,8}(\.\d{1,2})?$" ForeColor="Red"></asp:RegularExpressionValidator>
         <br />
         <br />
         <asp:Button ID="BtnTransfer" runat="server" OnClick="BtnTransfer_Click" Text="Confirm Transfer" />
 &nbsp;<asp:Label ID="lblSuccess" runat="server" Visible="False"></asp:Label>
         <br />
-        <asp:Button ID="BackButton" runat="server" OnClick="BackButton_Click" Text="Back" />
+        <asp:Button ID="BackButton" runat="server" OnClick="BackButton_Click" Text="Back" CausesValidation="False" />
             <br />
         <div>
             <asp:HiddenField ID="hdfTransactionDate" runat="server" />
--- TransferMoney.aspx.cs
+++ TransferMoney.aspx.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -53,32 +54,105 @@ public partial class TransferMoney : System.Web.UI.Page
         decimal customerBalance = (decimal)Session["CustomerBalance"];
         lblBalance.Text = customerBalance.ToString();
         decimal newBalance = customerBalance - amountToTransfer;
+        bool accountExists = CheckPayeeAccountExists(PayeeAccount);
+        int recipientAccount = int.Parse(accNumber.Text);
+        int recipientCustomerID = GetRecipientCustomerID(recipientAccount);
+        decimal recipientCurrentBalance = GetCurrentBalance(recipientAccount);
 
+        if (accountExists)
+        {
+            // Insert information into transaction table for current session user 
+            hookUp = new SqlConnection("Server=LAPTOP-11MN0H02\\SQLEXPRESS;Database=BankingApp;Integrated Security=True");
+            sql = "INSERT INTO dbo.TransactionTable (customerID, payeeAccount, transactionType, transactionDate, debit, balance)" +
+                           "VALUES (@CustomerID, @PayeeAccount, @TransactionType, @TransactionDate, @Debit, @Balance)";
+            sqlCmd = new SqlCommand(sql, hookUp);
+            sqlCmd.Parameters.AddWithValue("@CustomerID", customerID);
+            sqlCmd.Parameters.AddWithValue("@PayeeAccount", PayeeAccount);
+            sqlCmd.Parameters.AddWithValue("@TransactionType", transactionType);
+            sqlCmd.Parameters.AddWithValue("@TransactionDate", transactionDate);
+            sqlCmd.Parameters.AddWithValue("@Debit", amountToTransfer);
+            sqlCmd.Parameters.AddWithValue("@Balance", newBalance);
+            hookUp.Open();
+            sqlCmd.ExecuteNonQuery();
+            hookUp.Close();
+
+            // Update receiver's account balance 
+            hookUp = new SqlConnection("Server=LAPTOP-11MN0H02\\SQLEXPRESS;Database=BankingApp;Integrated Security=True");
+            sql = "UPDATE dbo.customerDetails SET customerBalance = customerBalance + @Credit WHERE customerAccount = @RecipientAccount;";
+            sqlCmd = new SqlCommand(sql, hookUp);
+            sqlCmd.Parameters.AddWithValue("@Credit", amountToTransfer);
+            sqlCmd.Parameters.AddWithValue("@RecipientAccount", recipientAccount);
+            hookUp.Open();
+            sqlCmd.ExecuteNonQuery();
+            hookUp.Close();
+
+            // Insert information into transaction table for receiver 
+            sql = "INSERT INTO dbo.TransactionTable (customerID, payeeAccount, transactionType, transactionDate, credit, balance)" +
+              "VALUES (@CustomerID, @PayeeAccount, @TransactionType, @TransactionDate, @Credit, @Balance)";
+            sqlCmd = new SqlCommand(sql, hookUp);
+            sqlCmd.Parameters.AddWithValue("@CustomerID", recipientCustomerID);
+            sqlCmd.Parameters.AddWithValue("@PayeeAccount", recipientAccount);
+            sqlCmd.Parameters.AddWithValue("@TransactionType", transactionType);
+            sqlCmd.Parameters.AddWithValue("@TransactionDate", transactionDate);
+            sqlCmd.Parameters.AddWithValue("@Credit", amountToTransfer);
+            sqlCmd.Parameters.AddWithValue("@Balance", (recipientCurrentBalance + amountToTransfer));
+            hookUp.Open();
+            sqlCmd.ExecuteNonQuery();
+            hookUp.Close();
+
+            // Update current session user's account balance 
+            hookUp = new SqlConnection("Server=LAPTOP-11MN0H02\\SQLEXPRESS;Database=BankingApp;Integrated Security=True");
+            sql = "UPDATE dbo.customerDetails SET customerBalance = customerBalance - @Debit WHERE customerID = @CustomerID;";
+            sqlCmd = new SqlCommand(sql, hookUp);
+            sqlCmd.Parameters.AddWithValue("@CustomerID", customerID);
+            sqlCmd.Parameters.AddWithValue("@Debit", amountToTransfer);
+            hookUp.Open();
+            sqlCmd.ExecuteNonQuery();
+            hookUp.Close();
+            lblSuccess.Text = "Data submitted successfully!";
+            lblSuccess.Visible = true;
+            Session["CustomerBalance"] = newBalance;
+            Response.Redirect("Home.aspx");
+        }
+        else
+        {
+            lblAccValid.Text = "Account does not exist";
+            lblAccValid.Visible = true;
+        }
+    }
+    
+    private bool CheckPayeeAccountExists(int PayeeAccount) // Check if user entered account exists
+    {
         hookUp = new SqlConnection("Server=LAPTOP-11MN0H02\\SQLEXPRESS;Database=BankingApp;Integrated Security=True");
-        sql = "INSERT INTO dbo.TransactionTable (customerID, payeeAccount, transactionType, transactionDate, debit)" +
-                       "VALUES (@CustomerID, @PayeeAccount, @TransactionType, @TransactionDate, @Debit)";
+        sql = "SELECT COUNT(*) FROM dbo.customerDetails WHERE customerAccount = @PayeeAccount";
         sqlCmd = new SqlCommand(sql, hookUp);
-        sqlCmd.Parameters.AddWithValue("@CustomerID", customerID);
         sqlCmd.Parameters.AddWithValue("@PayeeAccount", PayeeAccount);
-        sqlCmd.Parameters.AddWithValue("@TransactionType", transactionType);
-        sqlCmd.Parameters.AddWithValue("@TransactionDate", transactionDate);
-        sqlCmd.Parameters.AddWithValue("@Debit", amountToTransfer);
         hookUp.Open();
-        sqlCmd.ExecuteNonQuery();
-        hookUp.Close();
+        int count = (int)sqlCmd.ExecuteScalar(); // Get the count of matching accounts
+        return count > 0;
+    }
 
-
+    private int GetRecipientCustomerID(int payeeAccount)
+    {
         hookUp = new SqlConnection("Server=LAPTOP-11MN0H02\\SQLEXPRESS;Database=BankingApp;Integrated Security=True");
-        sql = "UPDATE dbo.customerDetails SET customerBalance = customerBalance - @Debit WHERE customerID = @CustomerID;";
+        sql = "SELECT customerID FROM dbo.customerDetails WHERE customerAccount = @PayeeAccount";
         sqlCmd = new SqlCommand(sql, hookUp);
-        sqlCmd.Parameters.AddWithValue("@CustomerID", customerID);
-        sqlCmd.Parameters.AddWithValue("@Debit", amountToTransfer);
+        sqlCmd.Parameters.AddWithValue("@PayeeAccount", payeeAccount);
         hookUp.Open();
-        sqlCmd.ExecuteNonQuery();
+        int recipientCustomerID = (int)sqlCmd.ExecuteScalar(); // Get the recipient's customerID
         hookUp.Close();
-        lblSuccess.Text = "Data submitted successfully!";
-        lblSuccess.Visible = true;
-        Session["CustomerBalance"] = newBalance;
-        Response.Redirect("Home.aspx");
+        return recipientCustomerID;
+    }
+
+    private decimal GetCurrentBalance(int recipientAccount)
+    {
+        hookUp = new SqlConnection("Server=LAPTOP-11MN0H02\\SQLEXPRESS;Database=BankingApp;Integrated Security=True");
+        sql = "SELECT customerBalance FROM dbo.customerDetails WHERE customerAccount = @RecipientAccount";
+        sqlCmd = new SqlCommand(sql, hookUp);
+        sqlCmd.Parameters.AddWithValue("@RecipientAccount", recipientAccount);
+        hookUp.Open();
+        decimal currentBalance = (decimal)sqlCmd.ExecuteScalar();
+        hookUp.Close();
+        return currentBalance;
     }
 }
